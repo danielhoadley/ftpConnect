258cb0ad5e781d42124d9de04771436329a79cb8
Carrefax <daniel.hoadley@iclr.co.uk>
2017-06-28 13:02:29 +0100

    Update ftpConnect.py

--- ftpConnect.py
+++ ftpConnect.py
@@ -3,5 +3,5 @@ from ftplib import FTP
 ftp = FTP('hostname.goes.here')
 ftp.login(user='username', passwd='password')
 
-# Get a list of the directories on the FTP site to test the connection work
+# Get a list of the directories on the FTP site to test the connection works
 ftp.retrlines('LIST') 
